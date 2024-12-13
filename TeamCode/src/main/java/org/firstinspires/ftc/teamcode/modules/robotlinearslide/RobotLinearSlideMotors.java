cd492f2979ead16c25f56c730054aedcde3a4ddb
longuint <longuint@proton.me>
2024-12-12 22:07:27 -0600

    Fix warnings for Second Semester 2024 Rewrite

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/robotlinearslide/RobotLinearSlideMotors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/robotlinearslide/RobotLinearSlideMotors.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode.modules.robotlinearslide;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 public class RobotLinearSlideMotors {
-    public DcMotorEx LINEAR_SLIDE_MOTOR = null;
+    public DcMotorEx LINEAR_SLIDE_MOTOR;
     public RobotLinearSlideMotors(DcMotorEx linearSlideMotor) {
         this.LINEAR_SLIDE_MOTOR = linearSlideMotor;
     }
