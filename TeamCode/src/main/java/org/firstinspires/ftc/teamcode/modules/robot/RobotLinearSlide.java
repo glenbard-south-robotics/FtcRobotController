b9ed9f222eb8ffed90efd72528895e8e19445791
longuint <longuint@proton.me>
2025-01-16 12:18:45 -0600

    RobotLinearSlide: The setPower() and setPowerAuxiliary() have been merged into setPowers(). Fix: Delete incorrect documentation.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/robot/RobotLinearSlide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/robot/RobotLinearSlide.java
@@ -9,10 +9,6 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.teamcode.GlobalConstants;
 import org.firstinspires.ftc.teamcode.modules.CustomMathFunctions;
 
-/**
- * @name RobotLinearSlide
- * @description This class controls the motors found on the robot using human input.
- */
 public class RobotLinearSlide {
     private final DcMotorEx LINEAR_SLIDE_MOTOR;
     private final DcMotorEx LINEAR_SLIDE_MOTOR_2;
@@ -26,32 +22,21 @@ public class RobotLinearSlide {
     }
 
     public void run() {
-        this.setPower(gamepadTwo.left_stick_y);
-        this.setPowerAuxiliary(gamepadTwo.left_stick_x);
+        this.setPowers(gamepadTwo.left_stick_y, gamepadTwo.left_stick_x);
     }
 
-    public void setPower(float position) {
-        position = CustomMathFunctions.clamp(-1, position * GlobalConstants.LINEAR_SLIDE_SENSITIVITY, 1);
-        if (this.LINEAR_SLIDE_MOTOR != null) {
-            this.LINEAR_SLIDE_MOTOR.setPower(position);
+    public void setPowers(float position1, float position2) {
+        position1 = CustomMathFunctions.clamp(-1, position1 * GlobalConstants.LINEAR_SLIDE_SENSITIVITY, 1);
+        position2 = CustomMathFunctions.clamp(-1, position2 * GlobalConstants.LINEAR_SLIDE_SENSITIVITY, 1);
+
+        if (this.LINEAR_SLIDE_MOTOR != null && this.LINEAR_SLIDE_MOTOR_2 != null) {
+            this.LINEAR_SLIDE_MOTOR.setPower(position1);
+            this.LINEAR_SLIDE_MOTOR_2.setPower(position2);
         } else {
-            throw new RuntimeException("LINEAR_SLIDE_MOTOR is null!");
+            throw new RuntimeException("A LINEAR_SLIDE_MOTOR is null!");
         }
     }
 
-    public void setPowerAuxiliary(float position) {
-        position = CustomMathFunctions.clamp(-1, position * GlobalConstants.LINEAR_SLIDE_SENSITIVITY, 1);
-        if (this.LINEAR_SLIDE_MOTOR_2 != null) {
-            this.LINEAR_SLIDE_MOTOR_2.setPower(position);
-        } else {
-            throw new RuntimeException("LINEAR_SLIDE_MOTOR_2 is null!");
-        }
-    }
-
-    /**
-     * @name setMotorPolicies()
-     * @description Changes the default policies of each motor.
-     */
     private void setMotorPolicies() {
         this.LINEAR_SLIDE_MOTOR.setDirection(DcMotorEx.Direction.FORWARD);
         this.LINEAR_SLIDE_MOTOR.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
