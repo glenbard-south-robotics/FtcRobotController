8ec7c4b64bb2b4a381f59d0e5ee9186544d156d5
languint <longuint@proton.me>
2025-09-08 17:43:09 -0500

    feat: initialize leftDrive in GBSBaseModule

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/robot/GBSBaseModule.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/robot/GBSBaseModule.kt
@@ -1,11 +1,21 @@
 package org.firstinspires.ftc.teamcode.modules.robot
 
+import com.qualcomm.robotcore.hardware.DcMotor
 import org.firstinspires.ftc.teamcode.modules.GBSModuleContext
 import org.firstinspires.ftc.teamcode.modules.GBSRobotModule
 
 class GBSBaseModule(context: GBSModuleContext) : GBSRobotModule(context) {
+    private lateinit var leftDrive: DcMotor
+
     override fun initialize(): Result<Unit> {
-        return Result.success(Unit)
+        return try {
+            leftDrive = context.hardwareMap.get(DcMotor::class.java, "leftDrive")
+            Result.success(Unit)
+        } catch (e: Exception) {
+            context.telemetry.addLine("An exception was raised in GBSBaseModule: ${e.message}")
+            context.telemetry.update()
+            Result.failure(e)
+        }
     }
 
     override fun run(): Result<Unit> {
