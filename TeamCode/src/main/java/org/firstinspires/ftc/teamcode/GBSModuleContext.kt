7a07f71c1ffcec06f6c158d8e2fe3fe987bfb51f
languint <longuint@proton.me>
2025-09-08 14:11:55 -0500

    refactor: GBSModuleContext.kt to be a data class with Telemetry and HardwareMap

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GBSModuleContext.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GBSModuleContext.kt
@@ -1,4 +1,6 @@
 package org.firstinspires.ftc.teamcode
 
-interface GBSModuleContext {
-}
+import com.qualcomm.robotcore.hardware.HardwareMap
+import org.firstinspires.ftc.robotcore.external.Telemetry
+
+data class GBSModuleContext(val hardwareMap: HardwareMap, val telemetry: Telemetry)
