e74f4e0cbc0dfc1884b22117eb8b43d1be21224d
languint <longuint@proton.me>
2025-09-08 17:48:53 -0500

    feat: finish initializing motors in GBSBaseModule.kt

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/robot/GBSBaseModule.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/robot/GBSBaseModule.kt
@@ -1,15 +1,21 @@
 package org.firstinspires.ftc.teamcode.modules.robot
 
 import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.hardware.DcMotorSimple
 import org.firstinspires.ftc.teamcode.modules.GBSModuleContext
 import org.firstinspires.ftc.teamcode.modules.GBSRobotModule
 
 class GBSBaseModule(context: GBSModuleContext) : GBSRobotModule(context) {
     private lateinit var leftDrive: DcMotor
+    private lateinit var rightDrive: DcMotor
 
     override fun initialize(): Result<Unit> {
         return try {
             leftDrive = context.hardwareMap.get(DcMotor::class.java, "leftDrive")
+            rightDrive = context.hardwareMap.get(DcMotor::class.java, "rightDrive")
+
+            leftDrive.direction = DcMotorSimple.Direction.REVERSE
+
             Result.success(Unit)
         } catch (e: Exception) {
             context.telemetry.addLine("An exception was raised in GBSBaseModule: ${e.message}")
