f062b9724a1772597518669e141114d292b54f2e
languint <longuint@proton.me>
2025-09-09 08:52:00 -0500

    fix: suppress PropertyName warnings in GBSConfiguration.kt

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GBSConfiguration.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GBSConfiguration.kt
@@ -2,9 +2,10 @@ package org.firstinspires.ftc.teamcode
 
 const val CYCLE_MS: Long = 50L
 
+@Suppress("PropertyName")
 class GBSBaseModuleConfiguration {
-     val BASE_POWER = 0.5
-     val FINE_ADJUST_POWER_COEFFICIENT = 0.1
-     val STICK_THRESHOLD = 0.2
-     val FINE_ADJUST_STICK_THRESHOLD = 0.05
+    val BASE_POWER = 0.5
+    val FINE_ADJUST_POWER_COEFFICIENT = 0.1
+    val STICK_THRESHOLD = 0.2
+    val FINE_ADJUST_STICK_THRESHOLD = 0.05
 }
