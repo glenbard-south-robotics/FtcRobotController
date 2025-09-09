566df3a7332f3fca53076e0520c8bd3dabb1809c
languint <longuint@proton.me>
2025-09-09 12:34:30 -0500

    refactor: initialize GBSHopperModule.kt

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/robot/GBSHopperModule.kt
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.modules.robot
+
+import org.firstinspires.ftc.teamcode.modules.GBSModuleContext
+import org.firstinspires.ftc.teamcode.modules.GBSRobotModule
+
+class GBSHopperModule(context: GBSModuleContext) : GBSRobotModule(context) {
+    override fun initialize(): Result<Unit> {
+        return super.initialize()
+    }
+
+    override fun run(): Result<Unit> {
+        TODO("Not yet implemented")
+    }
+
+    override fun shutdown(): Result<Unit> {
+        return super.shutdown()
+    }
+}
