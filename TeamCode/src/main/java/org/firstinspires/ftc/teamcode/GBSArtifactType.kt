b1d29cbaf54621ad04304faf212815d452dde756
languint <longuint@proton.me>
2025-09-09 08:50:54 -0500

    add: GBSMotif.kt add: GBSArtifactType.kt

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GBSArtifactType.kt
@@ -0,0 +1,6 @@
+package org.firstinspires.ftc.teamcode
+
+enum class GBSArtifactType {
+    GREEN,
+    PURPLE,
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GBSMotif.kt
@@ -0,0 +1,3 @@
+package org.firstinspires.ftc.teamcode
+
+typealias GBSMotif = Array<GBSArtifactType>
