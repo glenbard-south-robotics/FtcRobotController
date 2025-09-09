49fc24bd2f21e9105ee3175f934364742754a992
languint <longuint@proton.me>
2025-09-09 08:55:05 -0500

    refactor: move some file outside of modules/ feat: GBSBaseModule.kt add telemetry for initialization

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/GBSModuleContext.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/GBSModuleContext.kt
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.modules
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.hardware.HardwareMap
 import org.firstinspires.ftc.robotcore.external.Telemetry
+import org.firstinspires.ftc.teamcode.GBSGamepadPair
 
 data class GBSModuleContext(
     val opMode: LinearOpMode,
