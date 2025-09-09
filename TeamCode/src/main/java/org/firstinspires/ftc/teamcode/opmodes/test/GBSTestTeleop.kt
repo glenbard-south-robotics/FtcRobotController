da5819732983bdd2d4e0b851207c6daa31611664
languint <longuint@proton.me>
2025-09-08 20:25:04 -0500

    add: GBSTestTeleop.kt

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/test/GBSTestTeleop.kt
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.opmodes.test
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+
+@TeleOp(name = "GBSTestTeleop", group = "Tests")
+class GBSTestTeleop : LinearOpMode() {
+    override fun runOpMode() {
+        TODO("Not yet implemented")
+    }
+}
