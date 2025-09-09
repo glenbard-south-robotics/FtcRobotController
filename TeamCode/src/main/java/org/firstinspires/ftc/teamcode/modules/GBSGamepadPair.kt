8ed88ba528787815d79aaf0f7d3308873f675b31
languint <longuint@proton.me>
2025-09-08 20:37:10 -0500

    refactor: GBSConfiguration.kt to be a file of constants, and not a class. feat: Finish basic GBSTestTeleop.kt

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/GBSGamepadPair.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/GBSGamepadPair.kt
@@ -2,4 +2,4 @@ package org.firstinspires.ftc.teamcode.modules
 
 import com.qualcomm.robotcore.hardware.Gamepad
 
-data class GBSGamepadPair(val gamepadA: Gamepad, val gamepadB: Gamepad)
+data class GBSGamepadPair(val gamepad1: Gamepad, val gamepad2: Gamepad)
